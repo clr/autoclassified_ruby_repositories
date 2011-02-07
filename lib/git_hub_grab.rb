c0a1bc983c89907f60b45db10d05642ce1d143c1
CLR <clr@port49.com>
2011-02-06 21:13:06 -0500

    Tests passing for the method that gets the github repos.

--- lib/git_hub_grab.rb
+++ lib/git_hub_grab.rb
@@ -2,12 +2,12 @@ require 'nokogiri'
 require 'open-uri'
 
 class GitHubGrab
-  def grab_latest_updated(limit)
+  def self.grab_latest_updated(limit)
     links = []
     (1..limit).each do|i|
       url = "https://github.com/languages/Ruby/updated?page=#{i}"
       doc = Nokogiri::HTML(open(url))
-      links << doc.css(".title a:last").map{ |link| link.attribute('href').value}
+      links = doc.css(".title a:last").map{ |link| "https://github.com#{link.attribute('href').value}" }
     end
     links
   end
